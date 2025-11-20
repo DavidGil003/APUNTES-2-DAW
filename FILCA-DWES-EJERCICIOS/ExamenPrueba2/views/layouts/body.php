6ddc2ceb8da76e41d7078547f52f0857e799786d
filca532 <74068271+filca532@users.noreply.github.com>
2025-11-20 12:29:50 +0100

    Delete FILCA-DWES-EJERCICIOS/ExamenPrueba2 directory

--- FILCA-DWES-EJERCICIOS/ExamenPrueba2/views/layouts/body.php
+++
@@ -1,7 +0,0 @@
-<div class="content">
-    <div class="add-button-container">
-        <a href="index.php?action=add" class="btn btn-primary">➕ Agregar Nuevo Libro</a>
-    </div>
-
-    <?php include __DIR__ . '/../Libros/lista.php'; ?>
-</div>
--- FILCA-DWES-EJERCICIOS/ExamenPrueba2/views/layouts/footer.php
+++
@@ -1,8 +0,0 @@
-        <footer>
-            <p>&copy; 2025 Biblioteca de Filca. Todos los derechos reservados.</p>
-        </footer>
-    </div>
-    <script src="js/tema.js"></script>
-</body>
-
-</html>
